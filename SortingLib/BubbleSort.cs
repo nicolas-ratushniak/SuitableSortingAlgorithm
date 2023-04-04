4149155fe613b7964a0cab44e559587dd18a6e50
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-04 22:18:23 +0300

    Add Bubble sort

---
+++ SortingLib/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace SortingLib;
+
+public class BubbleSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                }
+            }
+        }
+    }
+}
