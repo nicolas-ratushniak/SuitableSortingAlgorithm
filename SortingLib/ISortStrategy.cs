11986c236aa65b217448792564673d22507d5d4f
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-04 22:17:25 +0300

    Add SortContext and fill ISortStrategy

--- SortingLib/ISortStrategy.cs
+++ SortingLib/ISortStrategy.cs
@@ -2,5 +2,5 @@
 
 public interface ISortStrategy
 {
-
+    public void Sort(int[] arr);
 }
---
+++ SortingLib/SortContext.cs
@@ -0,0 +1,21 @@
+﻿namespace SortingLib;
+
+public class SortContext
+{
+    private ISortStrategy _strategy;
+
+    public SortContext(ISortStrategy strategy)
+    {
+        _strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
+    }
+
+    public void ChangeStrategy(ISortStrategy strategy)
+    {
+        _strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
+    }
+
+    public void Sort(int[] arr)
+    {
+        _strategy.Sort(arr);
+    }
+}
