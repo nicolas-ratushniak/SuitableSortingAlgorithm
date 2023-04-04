c42e76ac52f89b3f98d758f6c82045d16e721890
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-04 22:22:53 +0300

    Add Insertion sort

---
+++ SortingLib/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿namespace SortingLib;
+
+public class InsertionSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int key = arr[i];
+            int comparerIndex = i - 1;
+
+            while (comparerIndex >= 0 && arr[comparerIndex] > key)
+            {
+                arr[comparerIndex + 1] = arr[comparerIndex];
+                comparerIndex--;
+            }
+            arr[comparerIndex + 1] = key;
+        }
+    }
+}
