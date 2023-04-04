396e2101053c150ee498aa3f10080771223f3d98
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-04 22:20:53 +0300

    Add Shaker sort

---
+++ SortingLib/ShakerSort.cs
@@ -0,0 +1,41 @@
+﻿namespace SortingLib;
+
+public class ShakerSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+
+        int begin = 0;
+        int end = arr.Length - 1;
+
+        while (begin < end)
+        {
+            int newBegin = end;
+            int newEnd = begin;
+
+            for (int i = begin; i < end; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+                    newEnd = i;
+                }
+            }
+            end = newEnd;
+
+            for (int i = end; i > begin; i--)
+            {
+                if (arr[i] < arr[i - 1])
+                {
+                    (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
+                    newBegin = i;
+                }
+            }
+            begin = newBegin;
+        }
+    }
+}
