c1fb47d262bbce4954dde977ab1d0cec26faf166
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-05 11:51:19 +0300

    Add Array Generator

---
+++ ArrayExtensionsLib/ArrayGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace ArrayExtensionsLib;
+
+public class ArrayGenerator
+{
+    private readonly Random _rand = new();
+
+    public void PopulateRandomly(int[] array, int min, int max)
+    {
+        if (array is null)
+        {
+            throw new ArgumentNullException(nameof(array));
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = _rand.Next(min, max + 1);
+        }
+    }
+}
