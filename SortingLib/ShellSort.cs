72deaafbd63695b38cbc12b9c38092e163cecd8b
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-05 07:14:17 +0300

    Add Shell sort

---
+++ SortingLib/ShellSort.cs
@@ -0,0 +1,35 @@
+﻿namespace SortingLib;
+
+public class ShellSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+
+        for (int gap = arr.Length / 2; gap > 0; gap /= 2)
+        {
+            for (int i = gap; i < arr.Length; i += 1)
+            {
+                // add a[i] to the elements that have
+                // been gap sorted save a[i] in temp and
+                // make a hole at position i
+                int temp = arr[i];
+
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                {
+                    arr[j] = arr[j - gap];
+                }
+                    
+                // put temp (the original a[i]) 
+                // in its correct location
+                arr[j] = temp;
+            }
+        }
+    }
+}
