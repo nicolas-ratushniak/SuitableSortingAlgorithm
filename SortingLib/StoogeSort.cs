ad6b2c5c63431d2afe53b983b0598661a8e51703
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-05 07:08:54 +0300

    Add Stooge sort

---
+++ SortingLib/StoogeSort.cs
@@ -0,0 +1,34 @@
+﻿namespace SortingLib;
+
+public class StoogeSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+
+        StoogeSortArray(arr, 0, arr.Length - 1);
+    }
+
+    private void StoogeSortArray(int[] arr, int low, int high)
+    {
+        if (low >= high)
+            return;
+
+        if (arr[low] > arr[high])
+        {
+            (arr[low], arr[high]) = (arr[high], arr[low]);
+        }
+
+        if (high - low + 1 > 2)
+        {
+            int oneThird = (high - low + 1) / 3;
+
+            StoogeSortArray(arr, low, high - oneThird);
+            StoogeSortArray(arr, low + oneThird, high);
+            StoogeSortArray(arr, low, high - oneThird);
+        }
+    }
+}
