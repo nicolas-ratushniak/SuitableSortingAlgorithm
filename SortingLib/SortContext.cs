5684536a7ed3f2495a427f053f775272efdbf01e
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-05 16:37:24 +0300

    Make Sort Context more flexible

--- SortingLib/SortContext.cs
+++ SortingLib/SortContext.cs
@@ -4,18 +4,24 @@ public class SortContext
 {
     private ISortStrategy _strategy;
 
+    public SortContext() { }
+
     public SortContext(ISortStrategy strategy)
     {
         _strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
     }
 
-    public void ChangeStrategy(ISortStrategy strategy)
+    public void SetStrategy(ISortStrategy strategy)
     {
         _strategy = strategy ?? throw new ArgumentNullException(nameof(strategy));
     }
 
     public void Sort(int[] arr)
     {
+        if (_strategy is null)
+        {
+            throw new InvalidOperationException("None of sort stratagies was chosen.");
+        }
         _strategy.Sort(arr);
     }
 }
