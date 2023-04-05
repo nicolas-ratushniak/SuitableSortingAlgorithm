f33e7b397d361bb9adc0c1de655ef67a13bece81
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-04-05 08:47:35 +0300

    Add Quick sort

---
+++ SortingLib/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿namespace SortingLib;
+
+public class QuickSort : ISortStrategy
+{
+    public void Sort(int[] arr)
+    {
+        if (arr is null)
+        {
+            throw new ArgumentNullException(nameof(arr));
+        }
+        QuickSortArray(arr, 0, arr.Length - 1);
+    }
+
+    static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+        }
+        (arr[i + 1], arr[high]) = (arr[high], arr[i + 1]);
+        return (i + 1);
+    }
+
+    private void QuickSortArray(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+
+            QuickSortArray(arr, low, pi - 1);
+            QuickSortArray(arr, pi + 1, high);
+        }
+    } 
+}
